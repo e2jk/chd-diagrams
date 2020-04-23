1ff4722d3d4870114c1ecd2444cdb8c4f5b365e7
Emilien Klein <emilien@klein.st>
2020-04-23 18:58:50 +0200

    Download images

--- chd-diagrams.py
+++ chd-diagrams.py
@@ -6,6 +6,7 @@
 import logging
 import os
 import urllib.request
+import urllib.parse
 import json
 
 def get_dumped_json(dump_file):
@@ -19,8 +20,23 @@ def get_dumped_json(dump_file):
         logging.debug("No dumped JSON file %s" % dump_file)
     return dumped_json
 
+def download_single_image(url, filepath):
+    logging.debug("Downloading %s to %s" % (url, filepath))
+    (filename, headers) = urllib.request.urlretrieve(url, filepath)
+
 def download_images(category, website_root, json_content):
     logging.debug("Downloading images for %s" % category)
+    output_folder = "output/%s" % category
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+    # First key is different in the 2 data sets:
+    # illustrationen vs. operationsIllustrationen
+    for i in json_content[list(json_content)[0]]:
+        name = i["name"]
+        bw_img = "%s%s" % (website_root, i["illustration_bw"])
+        ext = os.path.splitext(urllib.parse.urlparse(bw_img).path)[1]
+        filepath = os.path.join(output_folder, name + ext)
+        download_single_image(bw_img, filepath)
 
 def download_json(url):
     filename = "output/%s.json" % url.split("/")[-1]
